ae9fb04883e9bb8ee88804087412dfbaeef2618e
PrinceArora12 <153229054+PrinceArora12@users.noreply.github.com>
2026-03-09 18:16:54 +0530

    Update main.cpp

--- browser-history-manager/main.cpp
+++ browser-history-manager/main.cpp
@@ -1,1 +1,9 @@
-
+/*
+Project: Browser History Manager
+Author: Prince
+Language: C++
+Concepts: Stack, Classes, Arrays
+Description:
+A simple console application that simulates browser history.
+Users can visit pages, go back, and see the current page.
+*/
